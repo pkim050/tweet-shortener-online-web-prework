5427657d621a976284b665676dcdd8e71fa1e18b
Patrick Kim <pkim050@gmail.com>
2019-02-17 18:58:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def dictionary {
+  "hello" => "hi"
+  "to, two, too" => "2"
+  "for, four" => "4"
+  "be" => "b"
+  "you" => "u"
+  "at" => "@"
+  "and" => "&"
+}
+end
+
+def word_substituter(string)
+  arr = string.split
+  arr.each do |i|
+    if i == dictionary.keys
