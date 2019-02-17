8b981d7a685b968622b0d391fda0d03aaa749624
Patrick Kim <pkim050@gmail.com>
2019-02-17 19:12:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,15 @@
 # Write your code here.
 def dictionary {
-  "hello" => "hi"
-  "to, two, too" => "2"
-  "for, four" => "4"
-  "be" => "b"
-  "you" => "u"
-  "at" => "@"
-  "and" => "&"
+  :"hello" => "hi"
+  :"to" => "2"
+  :"two" => "2"
+  :"too" => "2"
+  :"for" => "4"
+  :"four" => "4"
+  :"be" => "b"
+  :"you" => "u"
+  :"at" => "@"
+  :"and" => "&"
 }
 end
 
